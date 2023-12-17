e432a70726b4e09ef7ad7e76840e30069253d7ed
Usenobong Usen Etokakpan <jabulanietokakpan@gmail.com>
2023-12-18 00:55:39 +0100

    Fixing alignments

--- src/Pages/LandingPage/Sections/Hero.jsx
+++ src/Pages/LandingPage/Sections/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
                 Decentralized Exchange Designed for  
                 <span className='text-[#0193FA]'> all Polygon</span> Network Users
             </h1>
-            <p className='lg:w-[480px] w-[380px] text-sm lg:text-base text-white mb-6 mt-2'>seamlessly exchange tokens via smart contracts and without the need for a custodial wallet to hold assets of the users.</p>
+            <p className='lg:w-[480px] w-[350px] text-sm lg:text-base text-white mb-6 mt-2'>seamlessly exchange tokens via smart contracts and without the need for a custodial wallet to hold assets of the users.</p>
             <div className="buttons flex gap-5">
                 <a href="https://benzswap-testnet.netlify.app/swap"><button className='w-[150px] justify-center p-3 hover:transition-[0.8s] hover:scale-[1.1] flex items-center rounded-3xl text-white bg-[#0374ef]'>Launch App</button></a>
                 <button className='w-[150px] justify-center p-3 hover:transition-[0.8s] hover:bg-[#0374ef]flex hover:bg-[#0374ef] hover:scale-[1.1] items-center rounded-3xl text-white bg-transparent border-[1px] border-[#0374ef]'>Explore</button>
